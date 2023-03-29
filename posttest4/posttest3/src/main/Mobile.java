b6f78425b751eef61ff7e2964c342c7725b1ceb4
Azharianto <114127352+Azharianto@users.noreply.github.com>
2023-03-29 23:37:11 +0800

    Add files via upload

---
+++ posttest4/posttest3/src/main/Mobile.java
@@ -0,0 +1,31 @@
+package main;
+
+public class Mobile {
+    int idMobile;
+    String Status;
+    public Mobile(String name,int price,int idmobile,String Status){
+        super();
+        this.idMobile = idmobile;
+        this.Status = Status;
+    }
+    public int getIdMobile() {
+        return idMobile;
+    }
+ 
+    public String getStatus() {
+        return Status;
+    }
+    public void setIdMobile(int idMobile) {
+        this.idMobile = idMobile;
+    }
+  
+    public void setStatus(String status) {
+        Status = status;
+    }
+    public void DataPenjualanProduk(String nama){
+    System.out.println("nama :" + nama);
+    }
+    public void DataPenjualanProduk(int harga){
+    System.out.println("harga :" + harga);
+    }
+}
